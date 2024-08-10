87075ec72a6007d223f24753790e6eb67b73a7c3
joe1-code <john4m2@gmail.com>
2024-08-10 13:20:14 +0300

    working on bars

---
+++ resources/views/layouts/includes/vertical_bar.blade.php
@@ -0,0 +1,27 @@
+<div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title" key="t-menu">Menu</li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-contributions">Contributions</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="" key="t-members">Members</a></li>
+                                    <li><a href="" key="t-payments">Monthly Payments</a></li>
+                                    <li><a href="" key="t-arrears">Monthly Arrears</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
